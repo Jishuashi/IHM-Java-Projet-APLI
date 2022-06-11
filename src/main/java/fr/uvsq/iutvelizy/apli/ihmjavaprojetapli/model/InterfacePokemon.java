6e2e91e2059dd68f1a5b28bf91abc35ba77b1382
darukity <samchgaming@gmail.com>
2022-06-11 21:07:20 +0200

    Refactored InterfaceCity to InterfacePokemon

---
+++ src/main/java/fr/uvsq/iutvelizy/apli/ihmjavaprojetapli/model/InterfacePokemon.java
@@ -0,0 +1,33 @@
+package fr.uvsq.iutvelizy.apli.ihmjavaprojetapli.model;
+
+public interface InterfacePokemon {
+    final String [] POKEMON_NAME = {
+            "Crocrodil" ,
+            "Volcaropod" ,
+            "Krabboss" ,
+            "Dynavolt" ,
+            "Balbuto" ,
+            "Kirlia" ,
+            "Machoc" ,
+            "Rattatacnote" ,
+            "Suicune" ,
+            "Cacturne" ,
+            "Tyranocifnote" ,
+            "Ortide" ,
+            "Lombre" ,
+            "Amonita" ,
+            "Colossinge" ,
+            "Latiasnote" ,
+            "Tylton" ,
+            "Smogo" ,
+            "Jungkonote" ,
+            "Arcaninnote" ,
+            "Deoxys" ,
+            "R�moraid" ,
+            "Piafabec" ,
+            "Hypoc�an" ,
+            "Reptincel" ,
+            "Scorplane" ,
+            "Carabaffe"
+    };
+}
